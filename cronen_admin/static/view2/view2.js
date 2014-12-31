dbc58ffe7bc5429920c19843ff4d648d5f0ac6d8
flend <flend@compsoc.net>
2014-12-31 17:20:05 +0000

    Add new servers on second page

--- cronen_admin/static/view2/view2.js
+++ cronen_admin/static/view2/view2.js
@@ -9,6 +9,32 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope', '$http', '_', function($scope, $http, _) {
 
-}]);
+    $scope.servers = {};
+    var refreshServerList = function() {
+
+      $http.get("api/server").success(function (data) {
+        $scope.servers = data.objects;
+      });
+    }
+
+    $scope.addServer = {};
+    $scope.addServer.hostname = '(hostname)';
+    $scope.addServer.port = '(port)';
+    $scope.addServer.submit = function(item, event) {
+
+      var addServerUri = 'api/server';
+      var payload = {
+        host: $scope.addServer.hostname,
+        port: $scope.addServer.port
+      };
+
+      console.log("POST " + addServerUri + JSON.stringify(payload));
+
+      $http.post(addServerUri, payload).success(function (data) {
+        refreshServerList();
+      })};
+
+    refreshServerList();
+  }]);
