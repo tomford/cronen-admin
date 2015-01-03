a1eb89dc0f870ecfe6b5223292cea7c09f351e38
flend <flend@compsoc.net>
2015-01-03 10:56:08 +0000

    Working simple unit test

--- cronen_admin/static/view1/view1_test.js
+++ cronen_admin/static/view1/view1_test.js
@@ -4,7 +4,7 @@ describe('View1 controllers', function() {
 
   var scope, ctrl, $httpBackend;
 
-  beforeEach(module('underscore'))
+  beforeEach(module('underscore'));
   beforeEach(module('myApp.view1'));
 
   beforeEach(inject(function (_$httpBackend_, $rootScope, $controller, underscore) {
@@ -14,7 +14,9 @@ describe('View1 controllers', function() {
     ctrl = $controller('View1Ctrl', {$scope: scope});
   }));
 
-  it('should popular one row in jobs for each server job', function() {
+  it('should popular one row in jobs for a server with one job', function() {
+
+    debugger;
 
     $httpBackend.expectGET('api/server').
       respond({
@@ -23,16 +25,33 @@ describe('View1 controllers', function() {
             host: "host1",
             id: 4,
             port: 666
-          },
-          {
-            host: "host2",
-            id: 5,
-            port: 999
           }
         ]
       });
 
-    //expect(Object.keys(scope.jobs).length).toBe(2);
-    expect(true).toBe(true);
+    $httpBackend.expectGET('api/status/4').
+      respond({
+        "host": "localhost",
+        "port": 12347,
+        "jobs": {
+          "hello-job": {
+            "start_time": "2014-01-01 12:30", "end_time": "2014-01-01 12:35", "error": "None"}
+        }});
+
+    console.log(scope.jobs);
+
+    $httpBackend.flush();
+
+    expect(Object.keys(scope.jobs).length).toBe(1);
+    /*
+    var expectedRow = {
+      "host": "localhost",
+      "port": 12347,
+      "jobs": {
+        "hello-job": {
+          "start_time": "2014-01-01 12:30", "end_time": "2014-01-01 12:35", "error": "None"}
+      }}
+
+    expect(Object.keys(scope.jobs)[0]).toBe()*/
   });
 });
