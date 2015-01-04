6ec7bb1f0a3e4eb5d3044346b198adbd0653de9c
flend <flend@compsoc.net>
2015-01-04 09:43:46 +0000

    First unit test for view2

--- cronen_admin/static/view2/view2_test.js
+++ cronen_admin/static/view2/view2_test.js
@@ -2,15 +2,61 @@
 
 describe('myApp.view2 module', function() {
 
+  var scope, ctrl, $httpBackend, _;
+
   beforeEach(module('myApp.view2'));
 
-  describe('view2 controller', function(){
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view2Ctrl = $controller('View2Ctrl', {$scope:{}});
-      expect(view2Ctrl).toBeDefined();
-    }));
+  beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
 
+    scope = $rootScope.$new();
+    _ = underscore;
+    ctrl = $controller('View2Ctrl', {$scope: scope});
+  }));
+
+  it('should popular one servers for each server returned from the server-side', function() {
+
+    $httpBackend.expectGET('api/server').
+      respond({
+        objects: [
+          {
+            host: "localhost",
+            id: 4,
+            port: 12347
+          },
+          {
+            host: "otherhost",
+            id: 5,
+            port: 999
+          }
+        ]
+      });
+
+    $httpBackend.flush();
+
+    expect(Object.keys(scope.servers).length).toBe(2);
+
+    var expectedServers = [
+      {
+        "host": "localhost",
+        "port": 12347,
+        "id": 4
+      },
+      {
+        "host": "otherhost",
+        "port": 999,
+        "id": 5
+      }
+      ];
+
+    expect(scope.servers).toEqualData(expectedServers);
   });
 });
