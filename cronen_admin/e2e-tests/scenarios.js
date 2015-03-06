4ae873eee7d7217a8c4e8ce0a3efe7ce65047623
tom <tom@europs.we7.local>
2015-03-06 16:17:46 +0000

    Tidying up top of integration tests

--- cronen_admin/e2e-tests/scenarios.js
+++ cronen_admin/e2e-tests/scenarios.js
@@ -1,12 +1,10 @@
 'use strict';
 
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
-
-describe('my app', function() {
-
-  browser.get('index.html');
+describe('Cronen admin', function() {
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+    browser.get('index.html');
+
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
   });
 
